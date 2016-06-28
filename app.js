1ecf2a189585f066516388a6dafb45834ac78f79
thanhvk <thanhvk.w3s@gmail.com>
2016-06-28 23:42:54 +0700

    Show list photos with dummy data

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var photos = require('./routes/photos');
 var users = require('./routes/users');
 
 var app = express();
@@ -13,6 +14,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+// directory photos setup
+app.set('photos', path.join(__dirname, 'public/photos'));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -23,6 +27,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/photos', photos);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
